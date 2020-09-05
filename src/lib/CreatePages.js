eed32cd565062a9f596b7dd2112ee9134e1d337a
윤지홍 <47561303+yoonjihong@users.noreply.github.com>
2020-09-06 02:10:23 +0900

    default layout update

--- src/lib/CreatePages.js
+++
@@ -1,3 +0,0 @@
-export async function CreatePages() {
-  console.log('==============test===============')
-}
--- src/pages/project.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Layout from "../components/layout"
-
-export default function project() {
-  return (
-    <div className="project">project</div>
-  )
-}
