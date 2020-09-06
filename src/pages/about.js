57e22896f50f757e2c542f382d5717b923bff10e
윤지홍 <47561303+yoonjihong@users.noreply.github.com>
2020-09-06 13:30:17 +0900

    update

--- src/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Layout from "../components/layout"
-
-export default function about() {
-  return (
-    <div className="about">about</div>
-  )
-}
--- src/posts/test.md
+++
@@ -1,9 +0,0 @@
----
-title: Gatsby로 간단한 git 블로그 만들기
-subtitle: 이것은 서브타이틀!?
-category: Develop & Design
----
-    
-# Hi22!
-    
-마크다운으로 작성한 게시글 입니다.
